cd561ace472a7492fc8b2cc86498f2932094361d
magoori <mgmillet3015@icloud.com>
2025-06-07 15:58:47 +0900

    8　指名のオートコンプリート

--- backend-ts/src/employee/EmployeeDatabaseInMemory.ts
+++ backend-ts/src/employee/EmployeeDatabaseInMemory.ts
@@ -6,9 +6,24 @@ export class EmployeeDatabaseInMemory implements EmployeeDatabase {
 
   constructor() {
     this.employees = new Map<string, Employee>();
-    this.employees.set("1", { id: "1", name: "Jane Doe", age: 22 });
-    this.employees.set("2", { id: "2", name: "John Smith", age: 28 });
-    this.employees.set("3", { id: "3", name: "山田 太郎", age: 27 });
+    this.employees.set("1", {
+      id: "1",
+      name: "Jane Doe",
+      furigana: "じぇーんどぅ",
+      age: 22,
+    });
+    this.employees.set("2", {
+      id: "2",
+      name: "John Smith",
+      furigana: "じょんすみす",
+      age: 28,
+    });
+    this.employees.set("3", {
+      id: "3",
+      name: "山田 太郎",
+      furigana: "やまだたろう",
+      age: 27,
+    });
   }
 
   async getEmployee(id: string): Promise<Employee | undefined> {
@@ -20,6 +35,8 @@ export class EmployeeDatabaseInMemory implements EmployeeDatabase {
     if (filterText === "") {
       return employees;
     }
-    return employees.filter((employee) => employee.name.includes(filterText));
+    return employees.filter((employee) =>
+      employee.furigana.startsWith(filterText)
+    );
   }
 }
