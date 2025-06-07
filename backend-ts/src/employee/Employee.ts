b1ed1467d4ac33453c3738688db483e10f380947
shelln <146703013+shun-osugi@users.noreply.github.com>
2025-06-07 14:29:57 +0900

    add preview Skills

--- backend-ts/src/employee/Employee.ts
+++ backend-ts/src/employee/Employee.ts
@@ -1,9 +1,10 @@
-import * as t from 'io-ts';
+import * as t from "io-ts";
 
 export const EmployeeT = t.type({
-    id: t.string,
-    name: t.string,
-    age: t.number,
+  id: t.string,
+  name: t.string,
+  age: t.number,
+  skills: t.array(t.string),
 });
 
 export type Employee = t.TypeOf<typeof EmployeeT>;
