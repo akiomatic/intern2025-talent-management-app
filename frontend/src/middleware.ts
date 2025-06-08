221782aba3f55f1fba5a5db943a443860c4210cd
akio <124953279+akiomatic@users.noreply.github.com>
2025-06-08 10:44:13 +0900

    refactor: update middleware matcher according to next-intl official guide

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -16,14 +16,8 @@ export function middleware(request: NextRequest) {
 }
  
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-  ],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 }
