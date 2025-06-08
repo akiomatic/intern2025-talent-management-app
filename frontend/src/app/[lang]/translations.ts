0a0ffb868977c5ccddd6ed3400994242146b4f13
akio <124953279+akiomatic@users.noreply.github.com>
2025-06-08 09:56:24 +0900

    refactor: rename dictionaries module to translations for clarity

--- frontend/src/app/[lang]/translations.ts
+++ frontend/src/app/[lang]/translations.ts
@@ -1,9 +1,9 @@
 import 'server-only'
 import { Locales } from '@/const/locales'
  
-const dictionaries = {
+const translations = {
   'en': () => import('@/dictionaries/en.json').then((module) => module.default),
   'ja': () => import('@/dictionaries/ja.json').then((module) => module.default),
 }
  
-export const getDictionary = async (locale: Locales) => dictionaries[locale]();
+export const getTranslations = async (locale: Locales) => translations[locale]();
