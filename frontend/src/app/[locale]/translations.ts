53b28c89454d4f987dd56f32f51a66a87f0da753
akio <124953279+akiomatic@users.noreply.github.com>
2025-06-08 11:02:41 +0900

    refactor: remove translations module as part of next-intl integration

--- frontend/src/app/[locale]/translations.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only'
-import { Locales } from '@/const/locales'
- 
-const translations = {
-  'en': () => import('@/dictionaries/en.json').then((module) => module.default),
-  'ja': () => import('@/dictionaries/ja.json').then((module) => module.default),
-}
- 
-export const getTranslations = async (locale: Locales) => translations[locale]();
